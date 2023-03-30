52ffdbe37ec2ae1c187328a38c7c7d63e727ffa8
Semion <semion.probochkin@yandex.ru>
2023-03-30 13:08:21 +0300

    Третье задание sorter

--- src/main/java/third/task/fileSort/Sorter.java
+++ src/main/java/third/task/fileSort/Sorter.java
@@ -10,17 +10,23 @@ import java.util.Scanner;
 public class Sorter {
 
     public File sortFile(File dataFile) throws IOException {
+        long countNumberInFiles;
+        long coutNumber = getTotalNumberOfLines(dataFile);
+        if (coutNumber < 50) {
+            countNumberInFiles = coutNumber / 2;
+        } else {
+            countNumberInFiles = coutNumber / 15;
+        }
 
-        long max = getTotalNumberOfLines(dataFile) / 10 ;
         int j = 0;
 
         try (FileInputStream inFile = new FileInputStream(dataFile)) {
 
             Scanner scanner = new Scanner(inFile);
             int i = 0;
-            List<Long> list = new ArrayList<Long>();
+            List<Long> list = new ArrayList<>();
             while (scanner.hasNextLong()) {
-                while (i < max && scanner.hasNextLong()) {
+                while (i < countNumberInFiles && scanner.hasNextLong()) {
                     list.add(scanner.nextLong());
                     i++;
                 }
@@ -37,10 +43,8 @@ public class Sorter {
                 list.clear();
                 i = 0;
                 j++;
-        }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            }
+        }catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -48,14 +52,14 @@ public class Sorter {
     }
 
     private static long getTotalNumberOfLines(File file) throws IOException {
-        long max = 0;
+        long max;
         try (FileInputStream inFile = new FileInputStream(file)) {
             Scanner scanner = new Scanner(inFile);
             max = scanner.findAll(System.lineSeparator()).count();
         }
         return max;
     }
-    private static File mergeSortfile (int end) throws FileNotFoundException {
+    private static File mergeSortfile (int end) {
 
         File result = null;
         int sortfilenumber = end - 1;
@@ -64,47 +68,83 @@ public class Sorter {
             File buffer1 = new File(String.format("data%d.txt", i));
             File buffer2 = new File(String.format("data%d.txt", 1 + i));
             result = mergeSortInnerfile(buffer1, buffer2, new File(String.format("data%d.txt", sortfilenumber)));
-            buffer1.delete();
-            buffer2.delete();
             i++;
             end++;
         }
 
         return result;
     }
-    private static  File mergeSortInnerfile(File file, File file2, File start) {
-        try(FileInputStream inFile = new FileInputStream(file);FileInputStream inFile2 = new FileInputStream(file2);PrintWriter pw = new PrintWriter(start)) {
+    public static  File mergeSortInnerfile(File file, File file2, File newFile) {
+        try(FileInputStream inFile = new FileInputStream(file);FileInputStream inFile2 = new FileInputStream(file2);PrintWriter pw = new PrintWriter(newFile)) {
             Scanner scannerfile1 = new Scanner(inFile);
             Scanner scannerfile2 = new Scanner(inFile2);
             if (scannerfile1.hasNextLong() && scannerfile2.hasNextLong()) {
                 Long number1 = scannerfile1.nextLong();
                 Long number2 = scannerfile2.nextLong();
                 while (scannerfile1.hasNextLong() && scannerfile2.hasNextLong()) {
+
+
                     if (number1 < number2) {
                         pw.println(number1);
+
                         number1 = scannerfile1.nextLong();
+
+                        if (!scannerfile1.hasNextLong()) {
+                            if (number1 < number2) {
+                                pw.println(number1);
+                                pw.println(number2);
+                            } else {
+                                pw.println(number2);
+                                while (scannerfile2.hasNextLong()) {
+                                    number2 = scannerfile2.nextLong();
+                                    if (number1 < number2) {
+                                        pw.println(number1);
+                                        pw.println(number2);
+                                        break;
+                                    } else {
+                                        pw.println(number2);
+                                        if (!scannerfile2.hasNextLong()) {
+                                            pw.println(number1);
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     } else {
                         pw.println(number2);
-                        if (scannerfile2.hasNextLong()) {
-                            number2 = scannerfile2.nextLong();
+                        number2 = scannerfile2.nextLong();
+                        if (!scannerfile2.hasNextLong()) {
+                            if (number2 < number1) {
+                                pw.println(number2);
+                                pw.println(number1);
+                            } else {
+                                pw.println(number1);
+                                while (scannerfile1.hasNextLong()) {
+                                    number1 = scannerfile1.nextLong();
+                                    if (number2 < number1) {
+                                        pw.println(number2);
+                                        pw.println(number1);
+                                        break;
+                                    } else {
+                                        pw.println(number1);
+                                        if (!scannerfile1.hasNextLong()) {
+                                            pw.println(number2);
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
+                }
 
+                while (scannerfile1.hasNextLong()) {
+                            pw.println(scannerfile1.nextLong());
                 }
-                if (scannerfile1.hasNextLong()) {
-                    pw.println(number1);
-                    while (scannerfile1.hasNextLong()) {
-                        number1 = scannerfile1.nextLong();
-                        pw.println(number1);
-                    }
+
+                while (scannerfile2.hasNextLong()) {
+                    pw.println(scannerfile2.nextLong());
                 }
-                if (scannerfile2.hasNextLong()) {
-                    pw.println(number2);
-                    while (scannerfile2.hasNextLong()) {
-                        number2 = scannerfile2.nextLong();
-                        pw.println(number2);
-                    }
-                }
+
             }
 
             scannerfile1.close();
@@ -114,7 +154,8 @@ public class Sorter {
         catch (IOException e) {
             e.printStackTrace();
         }
-
-        return start;
+        file.deleteOnExit();
+        file2.deleteOnExit();
+        return newFile;
     }
 }
--- src/main/java/third/task/fileSort/Test.java
+++ src/main/java/third/task/fileSort/Test.java
@@ -4,9 +4,9 @@ import java.io.*;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        File dataFile = new Generator().generate("data.txt", 100);
+        File dataFile = new Generator().generate("data.txt", 375);
         Sorter sorter = new Sorter();
-         System.out.println(new Validator(dataFile).isSorted()); // false
+        System.out.println(new Validator(dataFile).isSorted()); // false
         dataFile = sorter.sortFile(dataFile);
         System.out.println(new Validator(dataFile).isSorted()); // true
 
